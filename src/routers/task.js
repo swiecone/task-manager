ccf0030a6d112ac4852f01f0c7ff9c503a6c3a23
Usuario <usuario@MacBook-Air-de-Alex.local>
2021-09-05 20:40:01 +0200

    added parameter to filter true or false tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,9 +23,19 @@ router.post('/tasks', auth,  async (req, res) =>{
 
 //Get a list of all the tasks 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+        console.log(match.completed)
+
+    }
 
     try {
-          await req.user.populate('tasks').execPopulate()
+          await req.user.populate({
+              path: 'tasks', 
+              match
+          }).execPopulate()
         res.send(req.user.tasks)
 
        } catch (e) {
