c0114bbfefda5ff8105f71c6ea326ec5756afe1f
Alex <alex@alexswiec.com>
2021-10-24 19:22:11 +0200

    changed name of mongodb database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_LOCAL, {
+mongoose.connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true, 
     useCreateIndex: true,
     useFindAndModify: false     
